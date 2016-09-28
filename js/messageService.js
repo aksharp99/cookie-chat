cd3008e46946f7b0ddd86681c7ea00351042d6e5
akshar <aksharp99@gmail.com>
2016-09-28 14:23:00 -0600

    chat group

--- js/messageService.js
+++ js/messageService.js
@@ -1,6 +1,7 @@
 angular.module('chatroom').service('messageService', function($http){
   //Here you'll need to create two methods. One called postMessage and the other called getMessages.
 
+
   //On the lines below create a getMessages method. This method will retrieve data from the backend.
   //The url for the get request should be 'http://practiceapi.devmounta.in/api/chats'
   //Be sure to return whatever gets returned from $http so you can call .then in your controller.
@@ -14,7 +15,20 @@ angular.module('chatroom').service('messageService', function($http){
   //Also, remember that $http returns a promise. So if you return the whole $http call (return $http(...)), you can then use .then in your controller.
 
   //postMessage method here
-
+this.postMessage = function(yourMessage) {
+  return $http({
+    method: 'POST',
+    url: 'http://practiceapi.devmounta.in/api/chats',
+    message: yourMessage
+  })
+}
 
   //getMessages method here
+  this.getMessages = function() {
+    return $http({
+      method: 'GET',
+      url: 'http://practiceapi.devmounta.in/api/chats'
+    })
+  }
+
 });
